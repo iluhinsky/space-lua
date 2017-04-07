b04a033d276e65b49841abda0f6bfa10b233e1c9
Ilya Kompaniets <jenkslex@gmail.com>
2017-04-07 23:34:40 +0300

    Added face

--- bin/resources/shaders/particle_fragment.glsl
+++ bin/resources/shaders/particle_fragment.glsl
@@ -12,5 +12,5 @@ void main()
 {
     	float ration = estimatedTime/lifeTime;
 	vec4 alpha = texture2D(texture, vec2(TexCoord_.x, TexCoord_.y));
-	FragColor =  vec4(alpha.rgb+vec3(1.0, 1.0, 0.0)*(1-ration), 0.1 + ration);
+	FragColor =  vec4(alpha.rgb+vec3(1.0, 1.0, 0.0)*(1-ration), alpha.a * ration);
 }
